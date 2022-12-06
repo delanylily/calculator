57f5a4df322d95282e76c5d17bcb2e704c18b43a
lily <delanylily@gmail.com>
2022-12-06 14:13:51 +0100

    UI experience and spinner * Spinner added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-// listen for submit
-document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+document.querySelector('#loan-form').addEventListener('submit', function (e) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
 
-function calculateResults(form) {
+function calculateResults() {
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -11,36 +15,29 @@ function calculateResults(form) {
     const principal = parseFloat(amount.value);
     const calculatedInterest = parseFloat(interest.value / 100 / 12);
     const calculatedPayments = parseFloat(years.value * 12);
-
-    // monthly payments 
-
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
     const monthly = (principal * x * calculatedInterest) / (x - 1);
 
     if (isFinite(monthly)) {
         montlyPayment.value = monthly.toFixed(2);
-        // to Fixed sets decimals to 2
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers')
     }
-    form.preventDefault();
 }
 
 function showError(message) {
-    // get Elements 
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
     const errorAlert = document.createElement('div');
     errorAlert.className = 'alert alert-danger';
     errorAlert.appendChild(document.createTextNode(message));
-
-    // insert error above heading
     card.insertBefore(errorAlert, heading)
-
-    // clear error after 3 seconds
-
     setTimeout(clearError, 3000);
 }
 
